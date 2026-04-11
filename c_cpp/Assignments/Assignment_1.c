a1f5e8da3d27a63d526df39fbedd0d0a7e90e70e
Chaitanya Praveen Reddy <125331873+chaitanya2992@users.noreply.github.com>
2026-04-11 10:43:49 +0530

    Assignment_1

---
+++ c_cpp/Assignments/Assignment_1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+
+int a,b;
+
+printf("Enter 2 numbers to add them:\n");
+
+scanf("%d%d",&a,&b);
+
+int sum = a+b;
+
+printf("The sum of %d , %d is %d\n",a,b,sum);
+
+return 0;
+}
