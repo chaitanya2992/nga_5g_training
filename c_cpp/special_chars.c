5ff21a9336078a68c1a730137c5c679094e670da
Chaitanya Praveen Reddy <125331873+chaitanya2992@users.noreply.github.com>
2026-04-11 10:15:57 +0530

    C

---
+++ c_cpp/special_chars.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+
+printf("A line with \r coding\n");
+
+printf("A line with \t line tab \n");
+
+printf("third line using \t\t\t space\n");
+
+printf("a line using \b coding\n");
+
+return 0;
+}
