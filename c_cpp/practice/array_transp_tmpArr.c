286ad1f23c72ef9bf766793dea4a3e4499af4a96
Chaitanya Praveen Reddy <125331873+chaitanya2992@users.noreply.github.com>
2026-04-14 11:37:24 +0530

    2d arrays

---
+++ c_cpp/practice/array_transp_tmpArr.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+void input(int arr[][300], int rows, int cols){
+  printf("Enter %d sub arrays and %d values in each array: \n", rows, cols);
+ for(int i=0;i<rows;i++){
+    for(int j =0; j<cols; j++){
+        scanf("%u ", *(arr+i)+j);
+      }
+  }
+}
+
+void output(int tArr[][300], int arr[][300], int rows, int cols){
+  for(int i=0;i<rows;i++){
+    for(int j =0; j<cols; j++){
+        tArr[i][j] = arr[i][j];
+      }
+    }
+  printf("Array Values are: \n");
+  for(int i=0;i<cols;i++){
+    for(int j =0; j<rows; j++){
+        printf("%u ", *(*(arr+j)+i));
+      }
+      printf("\n");
+  }
+}
+int main(){
+  int rows, cols;
+  printf("No. Of rows: \n");
+  scanf("%d", &rows);
+  printf("No. Of Columns: \n");
+  scanf("%d", &cols);
+   
+  int arr[300][300];
+  int tArr[300][300];
+  input(arr, rows, cols);
+  output(tArr, arr, rows, cols);
+
+return 0;
+}
