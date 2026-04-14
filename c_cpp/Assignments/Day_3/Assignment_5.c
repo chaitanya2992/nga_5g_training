89bd15f89eece3266df2562bdbe5119798e83968
Chaitanya Praveen Reddy <125331873+chaitanya2992@users.noreply.github.com>
2026-04-14 22:12:12 +0530

    c assesment

---
+++ c_cpp/Assignments/Day_3/Assignment_5.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+input(int arr[], int n){
+  printf("Enter %d values: \n", n);
+  for(int i=0;i<n;i++){
+    scanf("%u", &arr[i]);
+  }
+}
+
+swap(int arr[], int n){
+  if(n%2 != 0){
+      n--;
+    }
+  for(int i=0;i<n;i+=2){
+    int temp = arr[i];
+    arr[i] = arr[i+1];
+    arr[i+1] = temp;
+  }
+}
+
+output(int arr[], int n){
+  printf("Array values after swap: \n");
+  for(int i=0;i<n;i++){
+    printf("%u ", arr[i]);
+  }
+  printf("\n");
+}
+
+int main(){
+
+  int n;
+  printf("Enter array length: \n");
+  scanf("%u", &n);
+  int *arr =(int *) calloc(n, sizeof(int));
+
+  input(arr, n);
+  swap(arr, n);
+  output(arr, n);
+
+  return 0;
+
+}
